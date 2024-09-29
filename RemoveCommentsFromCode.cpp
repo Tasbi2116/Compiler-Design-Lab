5502be59037c2300379ae9f1413719e324cb9a84
Tasbi2116 <tasbi2116@cseku.ac.bd>
2024-09-30 00:10:10 +0600

    keyword list down and special character list down code added

--- RemoveCommentsFromCode.cpp
+++ RemoveCommentsFromCode.cpp
@@ -1,59 +1,125 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
 bool multiple_comment = false;
 
-string Remove_comment(string string_line)
-{
+// Function to remove comments from the code
+string Remove_comment(string string_line) {
     bool single_comment = false;
     int string_size = string_line.length();
     string result_string = "";
-    // for traverse the whole string i need to use for loop
-    for(int i = 0; i<string_size; i++)
-    {
-        if(string_line[i] == '/' && string_line[i+1] == '/')
-        {
-            // means there is a single comment and so i need to make the single_comment flag true and the multile_comment flag = flase
+
+    for (int i = 0; i < string_size; i++) {
+        if (string_line[i] == '/' && string_line[i + 1] == '/') {
             single_comment = true;
             multiple_comment = false;
-            break; // hence there is a single line commnet so i dont need to check that line anymore.
-        }
-        else if(string_line[i] == '/' && string_line[i+1] == '*' && multiple_comment == false)
-        {
-            // when there is a multiple comment then there is first two character / and * so i have already read two character of the strig line so i can increase the i.
+            break; // Ignore rest of the line
+        } else if (string_line[i] == '/' && string_line[i + 1] == '*' && multiple_comment == false) {
             multiple_comment = true;
             i++;
-        }
-        else if(string_line[i] == '*' && string_line[i+1] == '/') // when a multiple line comment ends.
-        {
+        } else if (string_line[i] == '*' && string_line[i + 1] == '/') {
             multiple_comment = false;
             single_comment = false;
             i++;
-        }
-        else if(single_comment == false && multiple_comment == false)
-        {
-            // no comment so i have to save the exact code into the output.txt file by the result string return.
-            result_string+= string_line[i];
-
+        } else if (single_comment == false && multiple_comment == false) {
+            result_string += string_line[i]; // Add code that is not part of a comment
         }
     }
+
     return result_string;
 }
-int main()
-{
-    fstream fin, fout;
-    fin.open("input.txt");
-    fout.open("output.txt");
+
+int main() {
+    // Predefined keywords to search for in the code
+    set<string> predefinedWords = {"int", "float", "double", "if", "else", "for", "while", "return"};
+
+    // Special characters mapping
+    map<char, string> specialCharacters = {
+        {'#', "hash"},
+        {'<', "less than"},
+        {'>', "greater than"},
+        {'&', "ampersand"},
+        {'@', "at the rate of"},
+        {'%', "percent"},
+        {'$', "dollar"},
+        {'!', "exclamation mark"},
+        {'*', "asterisk"},
+        {'^', "Exponential character"},
+        {'?', "Question mark"},
+        {'.', "dot"},
+        {',', "comma"}
+    };
+
+    // Open input and output files
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    if (!fin) {
+        cerr << "Error opening input file!" << endl;
+        return 1;
+    }
 
     string string_line;
-    while (getline(fin, string_line))
-    {
-        string string_temp = Remove_comment(string_line);
-        fout<<string_temp<<endl;
-    }
+    set<string> matchedWords;      
+    set<char> matchedSpecialChars; 
+
+    fout << "Cleaned Code (without comments):\n\n";
     
+    while (getline(fin, string_line)) {
+        // Remove comments from each line
+        string cleaned_line = Remove_comment(string_line);
+        
+        // Write the cleaned code to the output file
+        fout << cleaned_line << endl;
+        
+        // Process the cleaned line for keywords and special characters
+        stringstream ss(cleaned_line);
+        string word;
+
+        while (ss >> word) {
+            // Check for keywords
+            if (predefinedWords.find(word) != predefinedWords.end()) {
+                matchedWords.insert(word); 
+            }
+
+            // Check for special characters in each word
+            for (char ch : word) {
+                if (specialCharacters.find(ch) != specialCharacters.end()) {
+                    matchedSpecialChars.insert(ch); 
+                }
+            }
+        }
+    }
+
     fin.close();
+
+    // Write keywords to the output file
+    fout << "\nPredefined keywords in the code:\n";
+    if (!matchedWords.empty()) {
+        for (const string &word : matchedWords) {
+            fout << word << endl;
+        }
+    } else {
+        fout << "No predefined keywords matched." << endl;
+    }
+
+    // Write special characters to the output file
+    fout << "\nSpecial characters in the code:\n";
+    if (!matchedSpecialChars.empty()) {
+        for (const char &ch : matchedSpecialChars) {
+            fout << ch << " - " << specialCharacters[ch] << endl;
+        }
+    } else {
+        fout << "No special characters matched." << endl;
+    }
+
     fout.close();
+
     return 0;
 }
